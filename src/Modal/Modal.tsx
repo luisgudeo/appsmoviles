2d73b851b2d804ec15cd0adc48ca9f13b4d30aac
Luis Fernando Girón <lgiron@bdgsa.net>
2024-09-23 08:14:16 -0600

    modal viernes

--- src/Modal/Modal.tsx
+++ src/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
-import { View, ScrollView, Modal, Text, Pressable } from 'react-native';
+import { View, ScrollView, Modal, Text, Pressable, Alert, TextInput } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome6';
 
 import { botones } from '../_estilos/botones';
 import { textos } from '../_estilos/textos';
@@ -7,15 +8,30 @@ import { views } from '../_estilos/views';
 
 const VistaModal = () => {
   const [verModal, setVerModal] = useState(false);
+  const [numero1, setNumero1] = useState('');
+  const [numero2, setNumero2] = useState('');
+  const sumar = () => {
+    let suma = parseInt(numero1) + parseInt(numero2);
+    setVerModal(false);
+    Alert.alert('El resultado es: ' + suma);
+  }
   return (
     <View>
       <Pressable style={[botones.btn]} onPress={() => { setVerModal(true); }}>
-        <Text style={[textos.btn]}>Presiona para ver el modal</Text>
+        <Text style={[textos.btn]}>Presiona para iniciar una suma</Text>
       </Pressable>
       <Modal animationType='fade' transparent={true} visible={verModal}>
         <View style={[views.bodyModal]}>
-          <Pressable style={[botones.btn]} onPress={() => { setVerModal(false); }}>
-            <Text style={[textos.btn]}>Presiona para cerrar el modal</Text>
+          <View style={[views.inputView]}>
+            <Icon name='user' size={24} color='black' solid/>
+            <TextInput placeholder='Primer numero' value={numero1} onChangeText={setNumero1} style={[views.input]} />
+          </View>
+          <View style={[views.inputView]}>
+            <Icon name='user' size={24} color='black' solid/>
+            <TextInput placeholder='Primer numero' value={numero2} onChangeText={setNumero2} style={[views.input]} />
+          </View>
+          <Pressable style={[botones.btn]} onPress={sumar}>
+            <Text style={[textos.btn]}>Realizar suma</Text>
           </Pressable>
         </View>
       </Modal>
